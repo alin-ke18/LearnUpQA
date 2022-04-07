d43ba614d727df052c2a4046ba58ad7899333097
Alina <phobosmoon2.0@gmail.com>
2022-04-08 01:15:27 +0300

    Adjustment

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] speed = {3, 5, 0};
+        int[] speed = {3, 0, 0};
         TrafficLights.setIsGreenLight(false);
         TrafficLights.getIsGreenLight();
-        TrafficLights.numberOfGetOut(speed);
-        Arrays.toString(TrafficLights.toKnowSpeedOut(speed));
+//        TrafficLights.numberOfGetOut(speed);
+//        Arrays.toString(TrafficLights.toKnowSpeedOut(speed));
         TrafficLights.toKnowStayIn(speed);
     }
 }
